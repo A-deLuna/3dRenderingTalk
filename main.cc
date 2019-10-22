5a3c84e60395c4fecafb543ee81a5d01cda0e5d8
tony <tonydelun@gmail.com>
2019-10-22 13:11:11 -0700

    code complete

--- main.cc
+++ main.cc
@@ -1,26 +1,25 @@
-#include "SDL.h"
 #include <cstdlib>
-#include "assimp/cimport.h"
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
-#include <string>
-#include <iostream>
-#include <cassert>
 #include <glm/glm.hpp>
-#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+#include "SDL.h"
+#include "assimp/cimport.h"
+#include "assimp/postprocess.h"
+#include "assimp/scene.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-using uint = unsigned int;
-using uchar = unsigned char;
+using glm::vec2;
+using glm::vec3;
+using glm::vec4;
 
 struct color {
-  unsigned char alpha;
-  unsigned char blue;
-  unsigned char green;
-  unsigned char red;
+  uint8_t alpha;
+  uint8_t blue;
+  uint8_t green;
+  uint8_t red;
 };
-color ColorRGB(uchar red, uchar green, uchar blue) {
+
+color ColorRGB(uint8_t red, uint8_t green, uint8_t blue) {
   color c;
   c.red = red;
   c.green = green;
@@ -29,149 +28,157 @@ color ColorRGB(uchar red, uchar green, uchar blue) {
   return c;
 }
 
-struct screenData {
+struct screen {
   color* framebuffer;
-  uchar* depthbuffer;
-  int width;
-  int height;
-  int depth;
+  uint8_t* depthbuffer;
+  int32_t width;
+  int32_t height;
+  uint8_t depth;
 };
 
 struct image {
-  uchar const* buffer;
-  int x; 
-  int y;
-  int channels;
+  uint8_t const* buffer;
+  int32_t x;
+  int32_t y;
+  int32_t channels;
 };
 
 struct resources {
-  aiScene const * scene; 
-  image* image; 
+  aiScene const* scene;
+  image* image;
 };
 
-void Initialize(SDL_Window** window, SDL_Renderer** renderer,
-                SDL_Texture** texture, screenData* screenData);
+void Initialize(SDL_Window** window,
+                SDL_Renderer** renderer,
+                SDL_Texture** texture,
+                screen* screen);
 
-glm::vec4 BoundingBox(glm::vec4 v1, glm::vec4 v2, 
-                      glm::vec4 v3) {
+vec4 BoundingBox(vec3 v1, vec3 v2, vec3 v3) {
   float minx = std::min(v1.x, std::min(v2.x, v3.x));
   float miny = std::min(v1.y, std::min(v2.y, v3.y));
   float maxx = std::max(v1.x, std::max(v2.x, v3.x));
   float maxy = std::max(v1.y, std::max(v2.y, v3.y));
 
-  return glm::vec4(minx, miny, maxx, maxy);
+  return vec4(minx, miny, maxx, maxy);
 }
 
-glm::vec3 Baricenter(glm::vec3 p, glm::vec3 v1, glm::vec3 v2, glm::vec3 v3) { 
-  glm::vec3 v21 = v2-v1;
-  glm::vec3 v31 = v3-v1;
-  glm::vec3 vp1 =  v1 - p;
+vec3 Baricenter(vec3 p, vec3 v1, vec3 v2, vec3 v3) {
+  vec3 v31 = v1 - v3;
+  vec3 v32 = v2 - v3;
+  vec3 pv3 = v3 - p;
 
-  glm::vec3 x = glm::vec3(v21.x, v31.x, vp1.x);
-  glm::vec3 y = glm::vec3(v21.y, v31.y, vp1.y);
-  glm::vec3 cross  = glm::cross(x,y);
+  vec3 x = vec3(v31.x, v32.x, pv3.x);
+  vec3 y = vec3(v31.y, v32.y, pv3.y);
+  vec3 cross = glm::cross(x, y);
 
   float u = cross.x / cross.z;
   float v = cross.y / cross.z;
 
-  return glm::vec3(1-u-v,u, v);
+  return vec3(u, v, 1.f - u - v);
 }
 
-bool PointInTriangle(glm::vec3 baricenter) {
-  return baricenter.x >= 0 && baricenter.y >= 0 && baricenter.x + baricenter.y <= 1.f;
+bool PointInTriangle(vec3 baricenter) {
+  return baricenter.x >= 0 && baricenter.y >= 0 &&
+         baricenter.x + baricenter.y <= 1.f;
 }
 
-void DrawTriangle(screenData* screenData, glm::vec4 v1, glm::vec4 v2, glm::vec4 v3, 
-                  glm::vec2 t1, glm::vec2 t2, glm::vec2 t3, image* image) { 
-  glm::vec4 bbox = BoundingBox(v1, v2, v3);
+void DrawTriangle(screen* screen,
+                  vec3 v1,
+                  vec3 v2,
+                  vec3 v3,
+                  vec3 normal,
+                  vec2 t1,
+                  vec2 t2,
+                  vec2 t3,
+                  image* image) {
+  vec4 bbox = BoundingBox(v1, v2, v3);
   float minx = bbox[0];
   float miny = bbox[1];
   float maxx = bbox[2];
   float maxy = bbox[3];
-  glm::vec3 lightDir(0, 0, 1);
-  glm::vec3 normal = glm::normalize(glm::cross(glm::vec3(v1-v2), glm::vec3(v1-v3)));
+  vec3 lightDir(0, 0, 1);
 
-  float magnitude = glm::dot(lightDir, normal);
-  if (magnitude < 0.f) return;
+  float magnitude = -glm::dot(lightDir, normal);
+  if (magnitude < 0.f)
+    return;
 
-  for (int y = miny; y <= maxy; y++) {
-    for (int x = minx; x <= maxx; x++) {
-      glm::vec3 baricenter = Baricenter(glm::vec3(x, y, 1), glm::vec3(v1), 
-                          glm::vec3(v2), glm::vec3(v3));
+  for (int32_t y = miny; y <= maxy; y++) {
+    for (int32_t x = minx; x <= maxx; x++) {
+      vec3 baricenter = Baricenter(vec3(x, y, 1), v1, v2, v3);
       if (PointInTriangle(baricenter)) {
-        glm::vec3 point = glm::vec3(v1 * baricenter.x + v2 * baricenter.y + v3 * baricenter.z);
-        uchar* pixelDepth = screenData->depthbuffer + (x + y * screenData->width);
-        if (point.z > *pixelDepth) {
-          *pixelDepth = point.z;
-          glm::vec2 textureCoords = t1 * baricenter.x + t2 * baricenter.y + t3 * baricenter.z;
-          glm::ivec2 screenTextCoords(textureCoords.x * image->x, (1.f-textureCoords.y) * image->y);
+        float pointz =
+            v1.z * baricenter.x + v2.z * baricenter.y + v3.z * baricenter.z;
+        uint8_t* pixelDepth = screen->depthbuffer + (x + y * screen->width);
+        if (pointz > *pixelDepth) {
+          *pixelDepth = pointz;
+          vec2 textureCoords =
+              t1 * baricenter.x + t2 * baricenter.y + t3 * baricenter.z;
+          glm::ivec2 screenTextCoords(textureCoords.x * image->x,
+                                      (1.f - textureCoords.y) * image->y);
 
-          uchar const* pixelData = &image->buffer[image->channels * (screenTextCoords.x + image->x * screenTextCoords.y)];
+          uint8_t const* pixelData =
+              &image->buffer[image->channels * (screenTextCoords.x +
+                                                image->x * screenTextCoords.y)];
           color color = ColorRGB(pixelData[0], pixelData[1], pixelData[2]);
-          screenData->framebuffer[x + y * screenData->width] = ColorRGB(
-            (uchar) (magnitude * 0xff),
-            (uchar) (magnitude * 0xff),
-            (uchar) (magnitude * 0xff)
-          );
+          screen->framebuffer[x + y * screen->width] =
+              ColorRGB((uint8_t)(magnitude * color.red),
+                       (uint8_t)(magnitude * color.green),
+                       (uint8_t)(magnitude * color.blue));
         }
       }
     }
   }
 }
 
-void Destroy(screenData* screenData, SDL_Window* window,
-             SDL_Renderer* renderer, SDL_Texture* texture);
+void Destroy(screen* screen,
+             SDL_Window* window,
+             SDL_Renderer* renderer,
+             SDL_Texture* texture);
 
-
-glm::vec4 assimpToGlm(const aiVector3D& aivert) {
-  return glm::vec4(aivert.x, aivert.y, aivert.z, 1.f);
+vec3 convertGlm(aiVector3D vec) {
+  return vec3(vec.x, vec.y, vec.z);
 }
 
-void Draw(screenData* screenData, resources* resources) {
-  static float rotation = 0.f;
-  rotation += .1f;
-  const aiMesh* mesh = resources->scene->mMeshes[0];
-  float depth = 255;
-  float w = screenData->width;
-  float h = screenData->height;
-  glm::mat4 rotateZ(-1, 0, 0, 0,
-                    0, -1 ,0, 0,
-                    0, 0, 1, 0,
-                    0,0,0,1);
-  glm::mat4 scale(w/2.0f,0,0,0,
-                  0,h/2.0f,0,0,
-                  0,0,depth/2.0f,0,
-                  0,0,0,1);
-  glm::mat4 translate(1,0,0,0,
-                      0,1,0,0,
-                      0,0,1,0,
-                      w/2.0f, h/2.0f, depth/2.0f, 1);
-  glm::mat4 rotateY = glm::rotate(glm::mat4(1), glm::radians(rotation), glm::vec3(0,1,0));
-  glm::mat4 viewport = translate * scale * rotateY * rotateZ;
+void Draw(screen* screen, resources* resources) {
+  aiMesh const* mesh = resources->scene->mMeshes[0];
 
-  for (int i = 0; i < mesh->mNumFaces; i++) {
+  for (size_t i = 0; i < mesh->mNumFaces; i++) {
     aiFace face = mesh->mFaces[i];
-    glm::vec4 v1 = viewport *
-      assimpToGlm(mesh->mVertices[face.mIndices[0]]);
-    glm::vec4 v2 = viewport *
-      assimpToGlm(mesh->mVertices[face.mIndices[1]]);
-    glm::vec4 v3 = viewport *
-      assimpToGlm(mesh->mVertices[face.mIndices[2]]);
 
-    glm::vec2 t1 = glm::vec2(mesh->mTextureCoords[0][face.mIndices[0]].x,
-                             mesh->mTextureCoords[0][face.mIndices[0]].y);
-    glm::vec2 t2 = glm::vec2(mesh->mTextureCoords[0][face.mIndices[1]].x,
-                             mesh->mTextureCoords[0][face.mIndices[1]].y);
-    glm::vec2 t3 = glm::vec2(mesh->mTextureCoords[0][face.mIndices[2]].x,
-                             mesh->mTextureCoords[0][face.mIndices[2]].y);
+    vec3 v1_model = convertGlm(mesh->mVertices[face.mIndices[0]]);
+    vec3 v1_screen = vec3((v1_model.x + 1.f) * screen->width / 2.f,
+                          (v1_model.y + 1.f) * screen->height / 2.f,
+                          (v1_model.z + 1.f) * screen->depth / 2.f);
 
-    DrawTriangle(screenData, v1, v2, v3, t1,t2,t3, resources->image);
+    vec3 v2_model = convertGlm(mesh->mVertices[face.mIndices[1]]);
+    vec3 v2_screen = vec3((v2_model.x + 1.f) * screen->width / 2.f,
+                          (v2_model.y + 1.f) * screen->height / 2.f,
+                          (v2_model.z + 1.f) * screen->depth / 2.f);
+
+    vec3 v3_model = convertGlm(mesh->mVertices[face.mIndices[2]]);
+    vec3 v3_screen = vec3((v3_model.x + 1.f) * screen->width / 2.f,
+                          (v3_model.y + 1.f) * screen->height / 2.f,
+                          (v3_model.z + 1.f) * screen->depth / 2.f);
+
+    vec3 normal =
+        glm::normalize(glm::cross(v3_model - v1_model, v2_model - v1_model));
+
+    vec2 t1 = vec2(mesh->mTextureCoords[0][face.mIndices[0]].x,
+                   mesh->mTextureCoords[0][face.mIndices[0]].y);
+    vec2 t2 = vec2(mesh->mTextureCoords[0][face.mIndices[1]].x,
+                   mesh->mTextureCoords[0][face.mIndices[1]].y);
+    vec2 t3 = vec2(mesh->mTextureCoords[0][face.mIndices[2]].x,
+                   mesh->mTextureCoords[0][face.mIndices[2]].y);
+
+    DrawTriangle(screen, v1_screen, v2_screen, v3_screen, normal, t1, t2, t3,
+                 resources->image);
   }
-
 }
 
-void EventLoop(screenData* screenData, resources* resources, 
-               SDL_Renderer* renderer, SDL_Texture* texture) {
+void EventLoop(screen* screen,
+               resources* resources,
+               SDL_Renderer* renderer,
+               SDL_Texture* texture) {
   bool running = true;
   while (running) {
     SDL_Event event;
@@ -185,87 +192,89 @@ void EventLoop(screenData* screenData, resources* resources,
     void* texturePixels;
     int pitch;
     SDL_LockTexture(texture, 0, &texturePixels, &pitch);
-    memset(screenData->framebuffer, 0x00,
-      screenData->height * screenData->width * sizeof(unsigned int));
+    memset(screen->framebuffer, 0x00,
+           screen->height * screen->width * sizeof(uint32_t));
 
-    memset(screenData->depthbuffer, 0x00,
-      screenData->height * screenData->width * sizeof(uchar));
+    memset(screen->depthbuffer, 0x00,
+           screen->height * screen->width * sizeof(uint8_t));
 
-    Draw(screenData, resources);
+    Draw(screen, resources);
 
-    memcpy(texturePixels, screenData->framebuffer,
-      screenData->height * screenData->width * sizeof(unsigned int));
+    memcpy(texturePixels, screen->framebuffer,
+           screen->height * screen->width * sizeof(uint32_t));
     SDL_UnlockTexture(texture);
-    SDL_RenderCopy(renderer, texture, 0, 0);
+    SDL_RenderCopyEx(renderer, texture, 0, 0, 0, 0, SDL_FLIP_VERTICAL);
     SDL_RenderPresent(renderer);
   }
 }
 
-
 int main() {
+  // Load model
   resources resources = {};
   resources.scene = aiImportFile("african_head/african_head.obj",
-                                  aiProcessPreset_TargetRealtime_Fast);
-  if(!resources.scene) {
+                                 aiProcessPreset_TargetRealtime_Fast);
+  if (!resources.scene) {
     std::cout << aiGetErrorString();
     exit(0);
   }
+
+  // Load texture
   image image = {};
   image.channels = 3;
   resources.image = &image;
-
   int imageChannels;
-  image.buffer = stbi_load("african_head/african_head_diffuse.tga",
-                               &image.x, &image.y, &imageChannels, 
-                               /*desired_channels=*/image.channels);
-  if(!image.buffer) {
+  image.buffer = stbi_load("african_head/african_head_diffuse.tga", &image.x,
+                           &image.y, &imageChannels,
+                           /*desired_channels=*/image.channels);
+  if (!image.buffer) {
     std::cout << stbi_failure_reason();
     exit(0);
   }
 
-  screenData screenData = {};
-  screenData.width = 1024;
-  screenData.height = 768;
+  screen screen = {};
+  screen.width = 1024;
+  screen.height = 768;
+  screen.depth = 255;
 
   SDL_Window* window;
   SDL_Renderer* renderer;
   SDL_Texture* texture;
-  Initialize(&window, &renderer, &texture, &screenData);
+  Initialize(&window, &renderer, &texture, &screen);
 
-  screenData.framebuffer = (color*)malloc(
-    screenData.height * screenData.width  * sizeof(color));
+  screen.framebuffer =
+      (color*)malloc(screen.height * screen.width * sizeof(color));
 
-  screenData.depthbuffer = (uchar*)malloc(
-    screenData.height * screenData.width *  sizeof(float));
+  screen.depthbuffer =
+      (uint8_t*)malloc(screen.height * screen.width * sizeof(uint8_t));
 
-  EventLoop(&screenData, &resources, renderer, texture);
- 
-  Destroy(&screenData, window, renderer, texture);
+  EventLoop(&screen, &resources, renderer, texture);
+
+  Destroy(&screen, window, renderer, texture);
   aiReleaseImport(resources.scene);
   stbi_image_free((void*)image.buffer);
 }
 
-void Initialize(SDL_Window** window, SDL_Renderer** renderer,
-                SDL_Texture** texture, screenData* screenData) {
+void Initialize(SDL_Window** window,
+                SDL_Renderer** renderer,
+                SDL_Texture** texture,
+                screen* screen) {
   SDL_Init(SDL_INIT_EVERYTHING);
-  *window = SDL_CreateWindow(
-    "title",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    screenData->width, screenData->height, 0);
+  *window = SDL_CreateWindow("title", SDL_WINDOWPOS_UNDEFINED,
+                             SDL_WINDOWPOS_UNDEFINED, screen->width,
+                             screen->height, 0);
 
   *renderer = SDL_CreateRenderer(*window, -1, 0);
 
-  *texture = SDL_CreateTexture(
-    *renderer,
-    SDL_PIXELFORMAT_RGBA8888,
-    SDL_TEXTUREACCESS_STREAMING,
-    screenData->width,screenData->height);
+  *texture = SDL_CreateTexture(*renderer, SDL_PIXELFORMAT_RGBA8888,
+                               SDL_TEXTUREACCESS_STREAMING, screen->width,
+                               screen->height);
 }
 
-void Destroy(screenData* screenData, SDL_Window* window,
-             SDL_Renderer* renderer, SDL_Texture* texture) {
-  free(screenData->framebuffer);
+void Destroy(screen* screen,
+             SDL_Window* window,
+             SDL_Renderer* renderer,
+             SDL_Texture* texture) {
+  free(screen->framebuffer);
   SDL_DestroyTexture(texture);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 default:
-	clang++ main.cc -I/usr/include/glm -I/usr/include/SDL2 -I/usr/include/assimp -lSDL2 -lassimp -O2
+	clang++ main.cc -I/usr/include/glm -I/usr/include/SDL2 -I/usr/include/assimp -lSDL2 -lassimp -O2 -Wall -Wextra
 
 release:
 	clang++ main.cc -I/usr/include/glm -I/usr/include/SDL2 -I/usr/include/assimp -lSDL2 -lassimp -O3
 dbg:
-	clang++ main.cc -I/usr/include/glm -I/usr/include/SDL2 -I/usr/include/assimp -lSDL2 -lassimp -O0 -g
+	clang++ main.cc -I/usr/include/glm -I/usr/include/SDL2 -I/usr/include/assimp -lSDL2 -lassimp -g
